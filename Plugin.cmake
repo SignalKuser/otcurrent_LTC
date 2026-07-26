5e8ebdccfe9e8eea18e616932179812480ccf0d0
C.Streicher <moody31@gmx.de>
2026-07-26 15:10:41 +0200

    Add files via upload

---
+++ Plugin.cmake
@@ -0,0 +1,112 @@
+# ~~~
+# Summary:      Local, non-generic plugin setup
+# Copyright (c) 2020-2021 Mike Rossiter
+# License:      GPLv3+
+# ~~~
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+
+# -------- Options ----------
+
+set(OCPN_TEST_REPO
+    "opencpn/otcurrent-alpha"
+    CACHE STRING "Default repository for untagged builds"
+)
+set(OCPN_BETA_REPO
+    "opencpn/otcurrent-beta"
+    CACHE STRING
+    "Default repository for tagged builds matching 'beta'"
+)
+set(OCPN_RELEASE_REPO
+    "opencpn/otcurrent-prod"
+    CACHE STRING
+    "Default repository for tagged builds not matching 'beta'"
+)
+
+#
+#
+# -------  Plugin setup --------
+#
+set(PKG_NAME otcurrent_pi)
+set(PKG_VERSION  4.6.5)
+set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
+
+set(DISPLAY_NAME otcurrent)    # Dialogs, installer artifacts, ...
+set(PLUGIN_API_NAME otcurrent) # As of GetCommonName() in plugin API
+set(PKG_SUMMARY "Predict future tidal currents")
+set(PKG_DESCRIPTION [=[
+Allows predictions of future tidal currents, based on the harmonics data in
+OpenCPN.
+]=])
+
+set(PKG_AUTHOR "Mike Rossiter")
+set(PKG_IS_OPEN_SOURCE "yes")
+set(PKG_HOMEPAGE https://github.com/Rasbats/otcurrent_pi)
+set(PKG_INFO_URL https://opencpn.org/OpenCPN/plugins/otcurrent.html)
+
+set(SRC
+        src/otcurrent_pi.h
+        src/otcurrent_pi.cpp
+        src/otcurrentOverlayFactory.cpp
+        src/otcurrentOverlayFactory.h
+        src/otcurrentUIDialogBase.cpp
+        src/otcurrentUIDialogBase.h
+        src/otcurrentUIDialog.cpp
+        src/otcurrentUIDialog.h
+        src/icons.h
+        src/icons.cpp
+        src/tcmgr.cpp
+        src/tcmgr.h
+        src/timectrl.cpp
+        src/timectrl.h
+        src/android/wx/datectrl.h
+        src/android/wx/timectrl.h
+        src/IDX_entry.cpp
+        src/IDX_entry.h
+        src/logger.cpp
+        src/logger.h
+        src/Station_Data.cpp
+        src/Station_Data.h
+        src/TC_Error_Code.h
+        src/TCDataFactory.cpp
+        src/TCDataFactory.h
+        src/TCDataSource.cpp
+        src/TCDataSource.h
+        src/TCDS_Ascii_Harmonic.cpp
+        src/TCDS_Ascii_Harmonic.h
+        src/TCDS_Binary_Harmonic.cpp
+        src/TCDS_Binary_Harmonic.h
+        src/georef.cpp
+        src/georef.h
+        src/plug_utils.cpp
+        src/plug_utils.h
+)
+
+set(PKG_API_LIB api-18)  #  A directory in libs/ e. g., api-17 or api-16
+
+macro(late_init)
+  # Perform initialization after the PACKAGE_NAME library, compilers
+  # and ocpn::api is available.
+  if (APPLE)
+    target_compile_definitions(${PACKAGE_NAME} PUBLIC OCPN_GHC_FILESYSTEM)
+  endif ()
+
+endmacro ()
+
+macro(add_plugin_libraries)
+  # Add libraries required by this plugin
+  add_subdirectory("${CMAKE_SOURCE_DIR}/libs/std_filesystem")
+  target_link_libraries(${PACKAGE_NAME} ocpn::filesystem)
+
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/plugin_dc")
+  target_link_libraries(${PACKAGE_NAME} ocpn::plugin-dc)
+
+  # The wxsvg library enables SVG overall in the plugin
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/wxsvg")
+  target_link_libraries(${PACKAGE_NAME} ocpn::wxsvg)
+
+endmacro ()
--- README.md
+++ README.md
@@ -1,37 +1,14 @@
-# otcurrent_LTC_V.2.20
-modified opencpn plugin
 
-this plugin based on "otcurrent v.4.6.5" from Mike Rossiter find here:
-https://github.com/Rasbats/otcurrent_pi
+OTCurrent Plugin for OpenCPN
+============================
 
-reason for modification:
-the original plugin only allow the hardcoded filenames harmonics-dwf-20210110-free.tcd and  HARMONIC.IDX
-and more then one files are not possible.
--
-hardcoded details:
-in https://github.com/Rasbats/otcurrent_pi/blob/master/src/otcurrentUIDialog.cpp   line 219 and 220 you find something like this:
-_______________________________
-```
-void otcurrentUIDialog::LoadTCMFile() {
-wxString TCDir = m_FolderSelected;
-TCDir.Append(wxFileName::GetPathSeparator());
-wxLogMessage(_("Using Tide/Current data from: ") + TCDir);
+This plugin uses the alternative managed workflow.
 
-wxString default_tcdata0 = TCDir + _T("harmonics-dwf-20210110-free.tcd");
-wxString default_tcdata1 = TCDir + _T("HARMONIC.IDX");
+License
+-------
+The plugin code is licensed under the terms of the GPL v2.
 
-// if (!TideCurrentDataSet.GetCount()) {
-TideCurrentDataSet.Add(default_tcdata0);
-TideCurrentDataSet.Add(default_tcdata1);
-```
-________________________________
-
-i modified the plugin, you can use all filenames and  multiple selection
-some features will follow...
-
-
-<img width="1880" height="983" alt="nordsee" src="https://github.com/user-attachments/assets/75fe95a0-d8d5-4095-81b4-15bfac0c3ee1" />
-
-
-
+Acknowledgements
+----------------
+The author wishes to thank Dave Register for providing OpenCPN, from which this plugin was derived.
 
